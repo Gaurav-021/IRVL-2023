a3fb47ce43cda540619625743ddc95123a4ea42a
Gaurav-021 <63582549+Gaurav-021@users.noreply.github.com>
2023-07-27 22:26:50 -0500

    Delete build directory

--- build/navigation/navfn/catkin_generated/navfn-msg-extras.cmake.develspace.in
+++
@@ -1,2 +0,0 @@
-set(navfn_MESSAGE_FILES "")
-set(navfn_SERVICE_FILES "/home/gaurav/fetch_ws/src/navigation/navfn/srv/MakeNavPlan.srv;/home/gaurav/fetch_ws/src/navigation/navfn/srv/SetCostmap.srv")
--- build/navigation/rotate_recovery/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gaurav/fetch_ws/src/navigation/rotate_recovery
-# Build directory: /home/gaurav/fetch_ws/build/navigation/rotate_recovery
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
